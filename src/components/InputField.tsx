b155591fd9553c1b2050e4a2f22846bf7571bb26
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-19 14:35:33 +1000

    Corrected InputField to pass the props we want

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,33 @@
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
-import { useField } from 'formik';
-import React, { InputHTMLAttributes } from 'react'
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
-    name: string;
-    label: string;
-    placeholder: string;
+  name: string;
+  label: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
-    const [field, {error}] = useField(props);
-        return (
-            <FormControl isInvalid={!!error}>
-                <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-                <Input {...field} id={field.name} placeholder={props.placeholder} />
-                <FormErrorMessage>{error}</FormErrorMessage>
-            </FormControl>
-        );
-}
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _, // We're not using size, so stripping it off and renaming it to _ as an unused prop
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+};
