6c36b262a6893b8408bbddc9bd8fa44e93cfce53
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-19 14:27:16 +1000

    Created InputField component

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,36 +5,43 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Box,
+  Button,
 } from "@chakra-ui/react";
-import { Wrapper } from '../components/Wrapper'
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   return (
-      <Wrapper>
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              value={values.username}
-              onChange={handleChange}
-              id="username"
+    <Wrapper>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
               placeholder="username"
-            />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
-</Wrapper>
+              label="Username"
+            ></InputField>
+            <Box mt={4}>
+              <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+              type="password"
+            ></InputField>
+          </Box>
+          <Button mt={4} isLoading={isSubmitting} type="submit" colorScheme="teal">register</Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
