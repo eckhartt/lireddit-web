d17d351a892c0cf9e53f2a5557f713c47255aa8f
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-19 14:57:45 +1000

    implement urql on register page to access graphql

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,16 +10,31 @@ import {
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
+const REGISTER_MUT = `mutation Register($username:String!,$password:String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
 
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
-          console.log(values);
+          register(values);
         }}
       >
         {({ isSubmitting }) => (
@@ -31,13 +46,20 @@ const Register: React.FC<registerProps> = ({}) => {
             ></InputField>
             <Box mt={4}>
               <InputField
-              name="password"
-              placeholder="password"
-              label="Password"
-              type="password"
-            ></InputField>
-          </Box>
-          <Button mt={4} isLoading={isSubmitting} type="submit" colorScheme="teal">register</Button>
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              register
+            </Button>
           </Form>
         )}
       </Formik>
