3a87d8c2f176fc86db6c582a64a274aa324a16ae
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-23 12:35:20 +1000

    Implemented @urql/exchange-graphcache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,84 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
 import theme from "../theme";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
+// Defining types we expect for cache.updateQuery
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+} 
+
+// Create urql client
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          // Every time login mutation runs this will update
+          // the cache of the MeQuery with new user data
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                // if login mutation returns error, return the existing MeQuery
+                // else update `me` to the returned user data
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          // Every time register mutation runs this will update
+          // the cache of the MeQuery with new user data
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                // if register mutation returns error, return the existing MeQuery
+                // else update `me` to the returned user data
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
-function MyApp({ Component, pageProps }) {
+// Rendering MyApp
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
