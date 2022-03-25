3fd7718165933bf787b48b49d69f05f9c1cdabbd
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-26 09:19:18 +1000

    Refactored urql client into separate utils

---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,78 @@
+import { dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import {
+  LogoutMutation,
+  MeQuery,
+  MeDocument,
+  LoginMutation,
+  RegisterMutation,
+} from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
+export const createUrqlClient = (ssrExchange: any) => ({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include" as const,
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          // Every time logout mutation runs this will update
+          // the cache of the MeQuery with null user data.
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              () => ({ me: null })
+            );
+          },
+          // Every time login mutation runs this will update
+          // the cache of the MeQuery with new user data.
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                // if login mutation returns error, return the existing MeQuery
+                // else update `me` to the returned user data.
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          // Every time register mutation runs this will update
+          // the cache of the MeQuery with new user data.
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                // if register mutation returns error, return the existing MeQuery
+                // else update `me` to the returned user data.
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
+});
