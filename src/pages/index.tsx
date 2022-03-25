41ffef8643f6e67b0748932ca13f9658b396c802
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-26 09:21:08 +1000

    Created initial posts graphql query and rendered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello world</div>
-  </>
-);
-export default Index;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>Hello world</div>
+      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(Index);
